dae7ef0a80814beab8079737da44f9f2e2ff665a
Joel-Jensen <info@joeljensen.se>
2022-03-30 14:18:08 +0200

    Initial commit

---
+++ app/src/main/java/se/lth/MAMN01/cykel_assistent/MainActivity.java
@@ -0,0 +1,14 @@
+package se.lth.MAMN01.cykel_assistent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
