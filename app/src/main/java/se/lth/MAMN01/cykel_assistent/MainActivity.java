77df647f55ea1f84192fd906b2d85131a8f98710
Lawand <la4515al-s@student.lu.se>
2022-05-11 12:03:01 +0200

    not Working Timer :(

--- app/src/main/java/se/lth/MAMN01/cykel_assistent/MainActivity.java
+++ app/src/main/java/se/lth/MAMN01/cykel_assistent/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+   public int speed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/se/lth/MAMN01/cykel_assistent/launchedApp.java
+++ app/src/main/java/se/lth/MAMN01/cykel_assistent/launchedApp.java
@@ -2,13 +2,50 @@ package se.lth.MAMN01.cykel_assistent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class launchedApp extends AppCompatActivity {
+    TextView tvTimer;
+    long startTime, timeInMilliseconds = 0;
+    Handler customHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launched_app);
+        tvTimer = (TextView) findViewById(R.id.tvTimer);
     }
+
+    public static String getDateFromMillis(long d) {
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return df.format(d);
+    }
+
+    public void start(View v) {
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread, 0);
+    }
+
+    public void stop(View v) {
+        customHandler.removeCallbacks(updateTimerThread);
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            tvTimer.setText(getDateFromMillis(timeInMilliseconds));
+            customHandler.postDelayed(this, 1000);
+        }
+    };
+
 }
